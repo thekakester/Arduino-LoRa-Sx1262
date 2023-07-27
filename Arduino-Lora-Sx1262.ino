9c2907558b2acad49760a5e0032917c4f63c3e8d
Mitch Davis <mitch@douglasinnovation.com>
2023-07-27 12:24:16 -0400

    Merge with main branch

--- Arduino-Lora-Sx1262.ino
+++ Arduino-Lora-Sx1262.ino
@@ -1,16 +1,21 @@
+/*License: CC 4.0 - Attribution, NonCommercial (by Mitch Davis, github.com/thekakester)
+* https://creativecommons.org/licenses/by-nc/4.0/   (See README for details)*/
 #include "LoraSx1262.h"
+
+byte* payload = "Hello world. This is mitch and I'm doing a Lora Test to see how this works";
+LoraSx1262* radio;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Booted");
 
-  LoraSx1262 radio = LoraSx1262();
+  radio = new LoraSx1262();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   Serial.println("Loop");
-  delay(1000);
+  radio->transmit(payload,strlen(payload));
 }
